bf42b1eeabbed73618c8da617720ad7f58980c5d
connor <kearnsco@mail.uc.edu>
2024-10-21 08:43:19 -0400

    Initial commit

---
+++ src/Task7.java
@@ -0,0 +1,12 @@
+public class Task7
+{
+    public static void main(String[] args) {
+        for(int cnt = 5; cnt >= 1; cnt--) {
+            for(int cnt2 = 1; cnt2 <= 5; cnt2++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
